bd460763d989c718a3d02e4e72c87a0884d643a5
ExQlusiv3 <wesley_hiraki@hotmail.com>
2020-02-10 18:01:36 +0100

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/**
+ * index.js
+ * This file contains the core of the application (aka bot). It will mostly only contain redirects to events.
+ * Commands will not be handled in this file to save ram memory.
+ * */
