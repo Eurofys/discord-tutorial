247abf8737150499bbddd427c3c772c36ac44835
DevShaded <fredriksundquist316@gmail.com>
2020-02-14 23:36:42 +0100

    edit: cleaned code and added a semi-colum

--- commands/public/coinflip.js
+++ commands/public/coinflip.js
@@ -5,7 +5,6 @@ module.exports = {
 	description: `Game: Coinflip. Which returns either heads or tails`,
 	execute: async function (client, message, args)
 	{
-
 		let coinArray = [
 			'Heads',
 			'Tails',
@@ -13,13 +12,11 @@ module.exports = {
 
 		let answer = Math.floor(Math.random() * coinArray.length);
 
-		await message.channel.send(
-		{
-			embed:
-			{
+		await message.channel.send({
+			embed:{
 				color: 0x0099ff,
 				description: `I flipped a coin and it turned out to be **${coinArray[answer]}**`,
 			}
 		});
 	}
-}
+};
