353d562ff2ea9d66068884f2aad8f2423c274be2
DevShaded <fredriksundquist316@gmail.com>
2020-02-14 22:15:57 +0100

    add: made a 8balll command

---
+++ commands/public/8ball.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+    name: '8ball',
+    description: `Game: 8ball, return a random message to the user`,
+    execute: async function (client, message, args){
+        let eightballArray = [
+            'It is certain', 
+            'It is decidedly so',
+            'Without a doubt', 
+            'Yes - definitely',
+            'You may rely on it', 
+            'As I see it, yes',
+            'Most likely',
+            'Outlook good',
+            'Yes',
+            'Signs point to yes',
+            'Reply hazy, try again',
+            'Ask again later',
+            'Better not tell you now',
+            'Cannot predict now',
+            'Concentrate and ask again',
+            'Dont count on it',
+            'My reply is no',
+            'My sources say no',
+            'Outlook not so good',
+            'Very doubtful'
+        ];
+
+        let answer = Math.floor(Math.random() * eightballArray.length);
+        
+
+        message.channel.send({
+            embed: {
+                color: 0x0099ff,
+                title: '8ball Game',
+                author: {
+                    name: message.author.username,
+                },
+                description: `🎱 <@${message.author.id}> ${eightballArray[answer]}`,
+            }
+        });
+    }
+}
