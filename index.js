b7536d570e91a1c1e8aef5d1a32c4a74f332f90b
Eurofys <Ville.themner@gmail.com>
2020-02-14 20:00:42 +0100

    init index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 'use strict';
 
-/**
- * index.js
- * This file contains the core of the application (aka bot). It will mostly only contain redirects to events.
- * Commands will not be handled in this file to save ram memory.
- * */
+// Defining Discord and including the Discord.js module
+const Discord = require('discord.js');
+const client = new Discord.Client({
+    partials: [
+        'MESSAGE',
+        'CHANNEL',
+        'REACTION'
+    ]
+});
+
+// Verify login and a steady connection to Discord their API
+client.on('ready', async () => {
+    console.log('\x1b[32mBot has succesfully signed in and is listening to events\x1b[0m');
+});
+
+// Request permissions to sign in the bot with Discord
+client.login('Njc3OTQ5Nzc5NDM4NzMxMjY0.Xkbsew.DH_Q0aTfVIXtCxbDjz5wwj4k_U4').then(() => {
+    console.log('\x1b[33mBot is trying to sign in\x1b[0m')
+}).catch(() => console.error('Api-token is invalid'));
