e1465f749a822f4b47bf6cb323dddddd99032e49
Eurofys <Ville.themner@gmail.com>
2020-02-14 20:25:49 +0100

    updated

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const dotenv = require('dotenv');
+
 // Defining Discord and including the Discord.js module
 const Discord = require('discord.js');
 const client = new Discord.Client({
@@ -16,6 +18,7 @@ client.on('ready', async () => {
 });
 
 // Request permissions to sign in the bot with Discord
-client.login('Njc3OTQ5Nzc5NDM4NzMxMjY0.XkbuzQ.ZAx33r8nZnHxzlFPvgmDS_wNxiA').then(() => {
-    console.log('\x1b[33mBot is trying to sign in\x1b[0m')
+dotenv.config();
+client.login(process.env.API_TOKEN).then(() => {
+    console.log('\x1b[33mBot is trying to sign in\x1b[0m');
 }).catch(() => console.error('Api-token is invalid'));
